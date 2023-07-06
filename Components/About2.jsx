f1863018751624fd7d7162ee5941174a484db53c
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-07-06 09:59:28 +0530

    some important updated on contact page

--- Components/About2.jsx
+++ Components/About2.jsx
@@ -9,9 +9,9 @@ const About2 = () => {
         <Image src="/img.jpg" alt="My Image" width={70} height={70} className="rounded-full"/>
         <div className="text-lg lg:text-xl">
             <p>Hey I am Mohit 😎</p> <br />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore repellendus enim odio earum recusandae ea similique facere explicabo ipsa ut.</p> <br />
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quisquam voluptates doloremque cumque similique expedita neque optio veniam hic ut laborum blanditiis a, quam odit laudantium non quaerat vitae minima voluptatum in repellat officia iste. Dolorem cupiditate tempore eum quisquam?</p><br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quas. Odio velit vel ex eligendi aut temporibus corrupti, error amet architecto, enim iure hic, explicabo porro a minus molestiae sunt?</p>
+            <p>I'm a Frontend Developer from India who is passionate about coding and building creative solutions. I started my journey by mastering HTML, CSS, and JavaScript. As I advanced, I enthusiastically adopted ReactJS as my main framework for projects.</p><br />
+            <p>Currently, I'm focused on utilizing NextJS to develop cutting-edge applications. I take pride in turning ideas into functional and visually appealing user interfaces. Always seeking growth and new challenges, I stay updated with the latest trends and technologies in frontend development.</p> <br />
+            <p>I thrive in collaborative team environments and communicate effectively with designers, backend developers, and other team members. I would be a valuable asset to any frontend development team.</p>
         </div>
         </div>
         <Stars/>
--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -1,17 +1,34 @@
-"use client"
-import { useState } from "react";
+
+import Image from "next/image";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { IoIosMail } from "react-icons/io";
+
 
 const Contact = () => {
-    const [isCircle, setIsCircle] = useState(false);
-
-    const handleButtonClick = () => {
-      setIsCircle(true);
-    };
-    
     return (
-        <div className="w-full h-screen flex-all2 dark-blue-bg">
-            <h2>How to Contact Me?</h2>
-            
+        <div className="w-full h-screen flex-all2 dark-blue-bg pb-10">
+            <h2 className="text-5xl font-bold mt-20 mb-5">Want to Hire Me?</h2>
+            <div>
+                <div className="contact-box p-5 flex flex-col gap-10 justify-center items-center px-20">
+                    <Image src="/img.jpg" alt="My Image" width={100} height={100} className="rounded-full" />
+                    <p className="text-center text-xl">Thank you very much for coming this far, now you have seen my work, so if you are interested, you can contact me from below links for <span className="underline">Project Request</span> or If you want to <span className="underline">Hire</span> me permanently as a developer.</p>
+                    <div className="text-4xl flex gap-10">
+                        <div className="border-2 p-3 rounded-full transition-all duration-300 cursor-pointer hover:bg-slate-100 hover:text-blue-800">
+                            <AiFillLinkedin />
+                        </div>
+                        <div className="border-2 p-3 rounded-full transition-all duration-300 cursor-pointer hover:bg-slate-100 hover:text-blue-400">
+                            <AiFillTwitterSquare />
+                        </div>
+                        <div className="border-2 p-3 rounded-full transition-all duration-300 cursor-pointer hover:bg-slate-100 hover:text-yellow-500">
+                            <IoIosMail />
+                        </div>
+                        <div className="border-2 p-3 rounded-full transition-all duration-300 cursor-pointer hover:bg-slate-100 hover:text-black">
+                            <AiFillGithub />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
